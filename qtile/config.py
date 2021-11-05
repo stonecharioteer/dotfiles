cb2e5e1111a30563062829d211374a46c963cf78
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-11-05 09:24:23 +0530

    Revert "add placeholder files for qtile config"
    This reverts commit 57c502ddce5b5723ab8c36e17a08f57d6dea2cd4.

--- qtile/config.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-try:
-    from qtile_common import *
-except ImportError:
-    sys.stderr.write("UNABLE TO LOAD CONFIG from qtile_common")
