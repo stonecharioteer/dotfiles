5c7fa80f08e15071ee75bd3c1d7837186eb92f46
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-11-15 15:35:14 +0530

    set --depth=1 to avoid getting all history

--- fonts/install.sh
+++ fonts/install.sh
@@ -5,9 +5,9 @@
 
 mkdir -p ~/.fonts
 cd ~/.fonts
-git clone https://github.com/hbin/top-programming-fonts.git
-git clone https://github.com/chrissimpkins/codeface.git
-git clone https://github.com/powerline/fonts.git
-git clone https://github.com/ryanoasis/nerd-fonts.git
+git clone --depth 1 https://github.com/hbin/top-programming-fonts.git
+git clone --depth 1 https://github.com/chrissimpkins/codeface.git
+git clone --depth 1 https://github.com/powerline/fonts.git
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 sudo fc-cache -f -v
 
